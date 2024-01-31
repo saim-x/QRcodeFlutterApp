cd2f5f9a1491fe745e1aeaaab2f7498ed9e02eab
saim-x <x_aci@hotmail.com>
2024-01-31 15:55:10 +0500

    sa

--- lib/scan_qr_code.dart
+++ lib/scan_qr_code.dart
@@ -49,44 +49,52 @@ class _ScanQRCodeState extends State<ScanQRCode> {
         backgroundColor: Color.fromARGB(255, 96, 0, 126),
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 30),
-            Text(
-              '$qrResult',
-              style: TextStyle(
-                color: Colors.black,
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w300,
-                fontSize: 18,
-              ),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: scanQR,
-              style: ElevatedButton.styleFrom(
-                primary: Colors.purple.shade400,
-                onPrimary: Colors.white,
-                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  side: BorderSide(color: Colors.purpleAccent.shade700),
-                ),
-                elevation: 5,
-              ),
-              child: Text(
-                'Scan QR Code',
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/background-image.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 30),
+              Text(
+                '$qrResult',
                 style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
                   fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w300,
+                  fontSize: 18,
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: scanQR,
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purple.shade400,
+                  onPrimary: Colors.white,
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: BorderSide(color: Colors.purpleAccent.shade700),
+                  ),
+                  elevation: 5,
+                ),
+                child: Text(
+                  'Scan QR Code',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
